28b1fce3c8bc83d40ee6b40528b624c78be9b1d7
Wijayaac <wijayaac25@gmail.com>
2020-04-15 14:08:19 +0800

    menambahkan judul halaman

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan</title>
+</head>
+
+<body>
+    <h1>Hello saya sedang belajar git dan menghubungkan tugas kuliah</h1>
+</body>
+
+</html>
